1a4b217e7af3571ce1d38a921fd91a60ad7bed37
erlint1212 <e.t.nordtvedt@gmail.com>
2024-04-10 21:28:36 +0200

    Added TextNode to LeafNode function and more tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,3 +1,5 @@
+from textnode import TextNode
+
 class HTMLNode():
     def __init__(self, TAG=None, VALUE=None, CHILDREN=None, PROPS=None):
         self.tag = TAG
@@ -10,7 +12,8 @@ class HTMLNode():
 
     def PROPS_TO_HTML(self):
         if self.props == None:
-            raise ValueError("Must input props, currently None")
+            return None
+            #raise ValueError("Must input props, currently None")
         if type(self.props) != dict:
             raise ValueError("Props must be dictionary")
         output = ""
@@ -18,5 +21,84 @@ class HTMLNode():
             output += f"{key}=\"{self.props[key]}\" "
         return output[:-1]
 
+    def _TO_HTML_CONVERTER(self, props_html):
+        if self.tag not in ["img", "code"]:
+            return f"<{self.tag}" + props_html + ">" + self.value + f"</{self.tag}>"
+        match self.tag:
+            case "img":
+                return f"<img" + props_html + ">"
+            case "code":
+                return f"```{self.value}```"
+            case _:
+                raise Exception("Tag not supported")
+
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, TAG=None, VALUE=None, PROPS=None):
+        if VALUE == None:
+            raise ValueError("Value is None, LeafNode requires a valid value")
+        super().__init__(TAG=TAG, VALUE=VALUE, PROPS=PROPS)
+    
+    def TO_HTML(self):
+        if self.value == None:
+            raise ValueError("Value is None, LeafNode requires a valid value")
+        if self.tag == None:
+            return self.value
+        
+        props_html = ""
+        if self.props != None:
+            props_html = " " + self.PROPS_TO_HTML()
+        
+        return self._TO_HTML_CONVERTER(props_html)
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, TAG=None, CHILDREN=None, PROPS=None):
+        if CHILDREN == None:
+            raise ValueError("CHILDREN are mandatory in ParentNode")
+        if type(CHILDREN) != list:
+            raise ValueError("CHILDREN must be a list")
+        if CHILDREN == []:
+            raise ValueError("There are no children in CHILDREN")
+        super().__init__(TAG=TAG, CHILDREN=CHILDREN, PROPS=PROPS)
+        self.value = None
+    
+    def TO_HTML(self):
+        output = ""
+        for child in self.children:
+            if isinstance(child, ParentNode):
+                output += child.TO_HTML()
+                continue
+            output += child.TO_HTML() 
+
+        self.value = output
+
+        props_html = ""
+        if self.props != None:
+            props_html = " " + child.PROPS_TO_HTML()
+
+        return self._TO_HTML_CONVERTER(props_html)
+
+def text_node_to_html_node(text_node):
+    if not isinstance(text_node, TextNode):
+        raise ValueError("Input must be a TextNode")
+    match text_node.text_type:
+        case "text":
+            return LeafNode(None, text_node.text)
+        case "bold":
+            return LeafNode("b", text_node.text)
+        case "italic":
+            return LeafNode("i", text_node.text)
+        case "code":
+            return LeafNode("code", text_node.text)
+        case "link":
+            return LeafNode("a", text_node.text, {"href":text_node.url})
+        case "image":
+            return LeafNode("img", "", {"src":text_node.url , "alt":text_node.text})
+        case _:
+            raise Exception(f"Text type \"{text_node.text_type}\" not supported")
+
+
+
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,13 +1,12 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import *
 
 
 class TestHTMLNode(unittest.TestCase):
     def test_PROPS_TO_HTML_None(self):
         node = HTMLNode()
-        with self.assertRaises(ValueError):
-            node.PROPS_TO_HTML()
+        self.assertEqual(node.PROPS_TO_HTML(), None)
         
     def test_PROPS_TO_HTML_NotDict(self):
         node = HTMLNode(PROPS=['Not','a', 'dict'])
@@ -26,6 +25,70 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode("h1", "test text", None, {"href": "https://www.google.com", "target": "_blank"})
         with self.assertRaises(NotImplementedError):
             node.TO_HTML()
+
+    # Test leafnode
+    def test_TO_HTML(self):
+        node = LeafNode("h1", "test text", {"href": "https://www.google.com", "target": "_blank"})
+        self.assertEqual(node.TO_HTML(), "<h1 href=\"https://www.google.com\" target=\"_blank\">test text</h1>")
+
+    def test_TO_HTML_no_props(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        self.assertEqual(node.TO_HTML(), "<p>This is a paragraph of text.</p>")
+    
+    # Test ParentNode
+    def test_ParentNode_TO_HTML_expected(self):
+        node = LeafNode("h1", "test1", {"href": "www.link.com"})
+        node1 = LeafNode("b", "test2")
+        node2 = LeafNode("p", "test3")
+        parent_node = ParentNode("div", CHILDREN=[node, node1, node2])
+        self.assertEqual(parent_node.TO_HTML(), "<div><h1 href=\"www.link.com\">test1</h1><b>test2</b><p>test3</p></div>")
+
+    def test_ParentNode_TO_HTML_nested(self):
+        with self.subTest():
+            node = LeafNode("h1", "test1", {"href": "www.link.com"})
+            node1 = LeafNode("b", "test2")
+            node2 = ParentNode("p", [node, node1])
+            parent_node = ParentNode("div", CHILDREN=[node2])
+            self.assertEqual(parent_node.TO_HTML(), "<div><p><h1 href=\"www.link.com\">test1</h1><b>test2</b></p></div>")
+
+        with self.subTest():
+            node = LeafNode("h1", "test1", {"href": "www.link.com"})
+            node1 = LeafNode("b", "test2")
+            node2 = ParentNode("p", [node, node1])
+            node3 = ParentNode("i", [node2])
+            parent_node = ParentNode("div", CHILDREN=[node3])
+            self.assertEqual(parent_node.TO_HTML(), "<div><i><p><h1 href=\"www.link.com\">test1</h1><b>test2</b></p></i></div>")
+
+    def test_ParentNode_NoChildren(self):
+        with self.assertRaises(ValueError):
+            node = ParentNode()
+
+    def test_ParentNode_EmptyChildren(self):
+        with self.assertRaises(ValueError):
+            node = ParentNode(None, None, [])
+
+    # TextNode to HTMLNode function tests
+    def test_TextNode_tags(self):
+        supported_tags = {
+                    "text" : None,
+                    "bold" : "b",
+                    "italic" : "i",
+                    "image" : "img",
+                    "link" : "a",
+                    "code" : "```"
+                }
+        for text_type in supported_tags.keys():
+            with self.subTest():
+                node = text_node_to_html_node(TextNode(f"testing {text_type}", text_type))
+                if text_type not in ["link", "image", "code"]:
+                    self.assertEqual(node.TO_HTML(), LeafNode(supported_tags[text_type], f"testing {text_type}").TO_HTML())
+                match text_type:
+                    case "link":
+                        self.assertEqual(node.TO_HTML(), f"<a href=\"None\">testing {text_type}</a>")
+                    case "image":
+                        self.assertEqual(node.TO_HTML(), f"<img src=\"None\" alt=\"testing {text_type}\">")
+                    case "code":
+                        self.assertEqual(node.TO_HTML(), f"```testing {text_type}```")
 
 if __name__ == "__main__":
     unittest.main()
